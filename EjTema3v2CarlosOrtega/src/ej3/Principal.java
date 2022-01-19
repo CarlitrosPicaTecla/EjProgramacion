3c9a3779b87c79af6c272a1ef616fce8b51a4806
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-01-20 00:13:27 +0100

    casa3

--- EjTema3v2CarlosOrtega/src/ej3/Principal.java
+++ EjTema3v2CarlosOrtega/src/ej3/Principal.java
@@ -1,5 +1,59 @@
 package ej3;
 
+import lectura.Leer;
+
 public class Principal {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Movil moviles[]=new Movil[10] ;
+		Movil movil;
+		Vendedor v=new Vendedor(moviles);
+		
+		int numMenu=0;
+	
+		 String marca;
+		String modelo;
+		double precio;
+
+	
+	do {
+		System.out.println("1.Añadir nuevo movil");
+		System.out.println("2.Vender movil");
+		System.out.println("3.Mostrar lista moviles");
+		System.out.println("4.Ofrecer cambio");
+		numMenu=Leer.datoInt();
+		
+		switch (numMenu) {
+		case 1:
+			System.out.println("Introduce marca del movil");
+			marca=Leer.dato();
+			System.out.println("Introduce modelo");
+			modelo=Leer.dato();
+			System.out.println("Itroduce precio");
+			precio=Leer.datoDouble();
+			movil= new Movil(marca,modelo,precio,false);			
+			int i=0;
+			v.addMovil(movil, numMenu);
+			break;
+		case 2:
+			
+			break;
+		case 3:
+		
+			int j=0;
+				v.mostarListaMovil(j, v);
+			
+
+			break;
+		case 4:
+			
+			break;
+
+		default:
+			break;
+		}
+	}while(numMenu!=0);
 }
+	}
--- EjTema3v2CarlosOrtega/src/ej3/Vendedor.java
+++ EjTema3v2CarlosOrtega/src/ej3/Vendedor.java
@@ -2,4 +2,34 @@ package ej3;
 
 public class Vendedor {
 
+	Movil moviles[];
+
+
+	public Vendedor(Movil[] moviles) {
+		super();
+		this.moviles = moviles;
+	}
+
+	public Movil[] getMoviles() {
+		return moviles;
+	}
+
+	public void setMoviles(Movil[] moviles) {
+		this.moviles = moviles;
+	}
+	
+	//METODOS
+	
+	public void addMovil(Movil movil, int i) {
+		moviles[i]=movil;
+	}
+	
+	public void mostarListaMovil(int i, Vendedor v) {
+		
+		System.out.println(v.getMoviles()[i].getMarca());
+		System.out.println(v.getMoviles()[i].getModelo());
+		System.out.println(v.getMoviles()[i].getPrecio());
+		System.out.println();
+	}
+	
 }
