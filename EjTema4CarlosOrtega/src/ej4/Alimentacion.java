f401de9f5e1567a5f98eb2384a54934e5bd63aac
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-02-08 14:25:09 +0100

    aeopo

--- EjTema4CarlosOrtega/src/ej4/Alimentacion.java
+++ EjTema4CarlosOrtega/src/ej4/Alimentacion.java
@@ -1,5 +1,36 @@
 package ej4;
 
-public class Alimentacion {
+public class Alimentacion extends Producto {
 
+
+	private int diasCaducidad;
+	
+	public Alimentacion(int id, double precioU, String nombre, int diasCaducidad) {
+		super(id, precioU, nombre);
+		this.diasCaducidad = diasCaducidad;
+	}
+	
+	
+
+	public int getDiasCaducidad() {
+		return diasCaducidad;
+	}
+
+
+
+
+	public void setDiasCaducidad(int diasCaducidad) {
+		this.diasCaducidad = diasCaducidad;
+	}
+
+
+
+
+
+	public double calcularPrecioFinal(double descuento) {
+		return 0;
+	}
+	
+	
+	
 }
--- EjTema4CarlosOrtega/src/ej4/Producto.java
+++ EjTema4CarlosOrtega/src/ej4/Producto.java
@@ -1,5 +1,49 @@
 package ej4;
 
-public class Producto {
+public abstract class Producto {
+	
+	private int id;
+	private double precioU;
+	private String nombre;
+	
+	
+	public Producto(int id, double precioU, String nombre) {
+		super();
+		this.id = id;
+		this.precioU = precioU;
+		this.nombre = nombre;
+	}
 
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public double getPrecioU() {
+		return precioU;
+	}
+
+
+	public void setPrecioU(double precioU) {
+		this.precioU = precioU;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public abstract double calcularPrecioFinal(double descuento);
+	
 }
