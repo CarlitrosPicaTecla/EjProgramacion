6ef726f82128d8034dc50ce8a063c654aed0aa76
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-02-21 10:51:12 +0100

    no veas

---
+++ EjemplosTema5/src/ejemplo1/Principal.java
@@ -0,0 +1,21 @@
+package ejemplo1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List <String>lista = new ArrayList<String>();
+		
+		lista.add("Carlos");
+		
+		System.out.println(lista);
+		
+		List<Integer>lista2 = new ArrayList<Integer>();
+		lista2.add(20);
+	}
+
+}
