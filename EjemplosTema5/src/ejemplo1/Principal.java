22dd833ad5f01d76c987d105181691d69a4a6507
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-02-22 14:25:06 +0100

    no veas chulo

--- EjemplosTema5/src/ejemplo1/Principal.java
+++ EjemplosTema5/src/ejemplo1/Principal.java
@@ -9,13 +9,26 @@ public class Principal {
 		// TODO Auto-generated method stub
 
 		List <String>lista = new ArrayList<String>();
+		Alumno a = new Alumno("Luis", 17, 7.8);
 		
 		lista.add("Carlos");
+		lista.add("Igna");
 		
 		System.out.println(lista);
 		
 		List<Integer>lista2 = new ArrayList<Integer>();
 		lista2.add(20);
+		
+		List<Alumno>listaA= new ArrayList<Alumno>();
+		
+		
+		listaA.add(new Alumno("Carlos", 18, 8.5));
+		listaA.add(a);
+		
+		System.out.println(listaA.get(1));
+		
+		
+		
 	}
 
 }
---
+++ EjemplosTema5/src/ejemploSet/Principal.java
@@ -0,0 +1,20 @@
+package ejemploSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Set <String> lista = new HashSet<String>();
+		
+		lista.add("Angel");
+		lista.add("Carlos");
+		lista.add("Paco");
+		
+		System.out.println(lista);
+	}
+
+}
