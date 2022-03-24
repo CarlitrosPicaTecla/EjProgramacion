e3cc433fe9c2053773698b32d90ddd33881b883f
CarlitrosPicaTecla <ortega.recar22@triana.salesianos.edu>
2022-03-24 10:36:19 +0100

    Ejercicios

---
+++ ProyectoOptionalCarlosOrtega/src/optional/Principal.java
@@ -0,0 +1,19 @@
+package optional;
+
+import java.util.Optional;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Usuario u = null;
+		
+		Optional<Usuario> optionalUser= Optional.ofNullable(u);
+	
+		Usuario usuarioFinal = optionalUser.orElse(new Usuario("01", "Carlos", "carlos@gmail.com"));
+		
+		System.out.println(usuarioFinal);
+	}
+
+}
